e71f00027ed840d04969d7cf36b784d08d6cde66
SoniTheBughehog <191974318+SoniTheBughehog@users.noreply.github.com>
2025-08-09 21:24:41 +0200

    callnote json

--- commands/callnote.js
+++ commands/callnote.js
@@ -1,7 +1,7 @@
 const fs = require('fs')
 const path = require('path')
 
-const filePath = path.join(__dirname, '..', 'data', 'appel.json')
+const filePath = path.join(__dirname, '..', 'data', 'callnote.json')
 
 function readCalls() {
   if (!fs.existsSync(filePath)) return []
