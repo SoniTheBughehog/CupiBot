e25bee2daa98e31a0e71cce6e8b4ad257210312a
SoniTheBughehog <191974318+SoniTheBughehog@users.noreply.github.com>
2025-08-22 11:44:06 +0200

    fix: config missing

--- commands/callnote.js
+++ commands/callnote.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 const path = require("path");
 const { EmbedBuilder } = require("discord.js");
 const { sendEmbed } = require("../utils/sendEmbed");
-const config = require("./config.json");
+const config = require("../config.json");
 
 const filePath = path.join(__dirname, "..", "data", "callnote.json");
 
